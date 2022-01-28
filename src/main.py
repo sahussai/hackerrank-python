a80b2a09688073fa6d594c85f0da7d1721151319
Syed Hussain <syed.hussain@telus.com>
2022-01-28 20:53:07 +0000

    Update main.py to run new solution

--- src/main.py
+++ src/main.py
@@ -1,6 +1,14 @@
-from leetcode_1512 import Solution
+from leetcode_1512 import Solution as lc_1512
+from leetcode_771 import Solution as lc_771
 
 
-array = [1,2,3,1,1,3]
-array2 = [1,1,1,1]
-print(Solution.numIdenticalPairs(array,array))
+''' Leetcode 1512 Number of Good Pairs'''
+# array = [1,2,3,1,1,3]
+# array2 = [1,1,1,1]
+# print(lc_1512.numIdenticalPairs(array,array))
+
+
+jewels = "aA"
+stones = "aAAbbbAAa"
+
+print(lc_771.numJewelsInStones(jewels, jewels, stones))
