41dc5f422fabfef099831cfb44a702c36b828f6f
Syed Hussain <syed.hussain@telus.com>
2022-01-29 03:15:11 +0000

    Leetcode 1365 How Many Numbers Are Smaller Than The Current Number

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from leetcode_1512 import Solution as lc_1512
 from leetcode_771 import Solution as lc_771
+from leetcode_1365 import Solution as lc_1365
+
 
 
 ''' Leetcode 1512 Number of Good Pairs'''
@@ -8,7 +10,15 @@ from leetcode_771 import Solution as lc_771
 # print(lc_1512.numIdenticalPairs(array,array))
 
 
-jewels = "aA"
-stones = "aAAbbbAAa"
+''' Leetcode 771 Jewels and Stones'''
+# jewels = "aA"
+# stones = "aAAbbbAAa"
 
-print(lc_771.numJewelsInStones(jewels, jewels, stones))
+# print(lc_771.numJewelsInStones(jewels, jewels, stones))
+
+
+''' Leetcode 1365 How Many Numbers Are Smaller Than the Current Number?'''
+
+array = [8,1,2,2,3]
+# array.insert()
+print(lc_1365.smallerNumbersThanCurrent(array,array))
